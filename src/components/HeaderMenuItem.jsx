9647d5d7a02d8ee93744ded06ecf4168c76beb9a
Jon Zubizarreta <jonzubi97@gmail.com>
2023-03-25 15:15:58 +0100

    Meto margen horizontal en los menu item

--- src/components/HeaderMenuItem.jsx
+++ src/components/HeaderMenuItem.jsx
@@ -1,6 +1,6 @@
 export default function HeaderMenuItem ({ section }) {
   return (
-    <span className='p-4 px-8 rounded-lg bg-slate-100 hover:bg-blue-500 hover:text-white text-center cursor-pointer font-bold'>
+    <span className='mx-4 p-4 px-8 rounded-lg bg-slate-100 hover:bg-blue-500 hover:text-white text-center cursor-pointer font-bold'>
       {section}
     </span>
   )
