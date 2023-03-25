b6adf6f4c04074257e26e18005cee433a7bfe80c
Jon Zubizarreta <jonzubi97@gmail.com>
2023-03-25 14:39:46 +0100

    Meto header como componente

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
-import DefaultHead from '@/components/defaultHead'
+import DefaultHead from '@/components/DefaultHead'
+import Header from '@/components/Header'
 import '@/styles/globals.css'
 
 export default function App ({ Component, pageProps }) {
   return (
     <>
       <DefaultHead />
+      <Header />
       <Component {...pageProps} />
     </>
   )
