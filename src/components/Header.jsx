f65662907c493ac8ebe5361a4b0976233f7bf53f
Jon Zubizarreta <jonzubi97@gmail.com>
2023-03-25 15:15:33 +0100

    Meto padding en header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Logo from './Logo'
 
 export default function Header () {
   return (
-    <header className='flex justify-around items-center'>
+    <header className='flex justify-around items-center p-3'>
       <Logo />
       <HeaderMenu />
     </header>
