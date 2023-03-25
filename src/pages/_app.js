9993190a253f44e4e8ee08d74f55a99f34862d15
Jon Zubizarreta <jonzubi97@gmail.com>
2023-03-25 15:19:06 +0100

    Cambio a ruta relativa

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
-import DefaultHead from '@/components/DefaultHead'
-import Header from '@/components/Header'
-import '@/styles/globals.css'
+import DefaultHead from '../components/DefaultHead'
+import Header from '../components/Header'
+import '../styles/globals.css'
 
 export default function App ({ Component, pageProps }) {
   return (
