87a327725bb3b6d2a35d63cf720ab03cac284eec
Jon Zubizarreta <jonzubi97@gmail.com>
2023-03-25 14:47:37 +0100

    Añado imagen al logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,12 @@
+import Image from 'next/image'
+
 export default function Logo () {
   return (
-    <div className='font-bold'>
-      Jon Zubizarreta
+    <div className='flex items-center'>
+      <Image className='rounded-3xl' alt='imagenPerfil' src='/imagenPerfil.jpeg' width={50} height='50' />
+      <span className='font-bold text-3xl ml-6'>
+        Jon Zubizarreta
+      </span>
     </div>
   )
 }
